1506aaa1af2c68c8ec211b11bb15e9a5754051fb
Jessica <rojotianarahajaharivony@gmail.com>
2024-03-26 13:28:07 +0100

    add a new file

--- stockageProfil.cpp
+++ stockageProfil.cpp
@@ -8,45 +8,70 @@ using namespace std;
 typedef vector<float> Vector;
 typedef vector<Vector> Matrix;
 
-class Factorisation{
+class Profil{
     private:
         Matrix A;
         Vector b;       //second membre
         Vector D;       //pour stocker les éléments diagonaux de la matrice L
+        Vector APi;     //Valeur de la matrice
+        Vector li;      //nombre de valeur avant la valeur du diagonal
+        Vector pi;      //i - li
+        Vector nDiag;   //numéro du diagonal
         int n;
 
     public:
-        Factorisation();
+        Profil();
         void displayMatrix(Matrix M);
         void displayVector(Vector V);
         void initializeData();
         Matrix get_matrix();
         Vector get_vector();
+        Vector get_APi();
         Vector get_solution();
         Vector get_diag();
+        Vector get_num_diag();
+        Vector get_li();
+        Vector get_pi();
         Vector solution;
         void decomposition();
         void lower_resolution();
         void upper_resolution();
+        void stockage();
 };
 
-Factorisation::Factorisation(){
+Profil::Profil(){
     this->initializeData();
 }
 
-Matrix Factorisation::get_matrix(){
+Matrix Profil::get_matrix(){
     return this->A;
 }
 
-Vector Factorisation::get_vector(){
+Vector Profil::get_vector(){
     return this->b;
 }
 
-Vector Factorisation::get_diag(){
+Vector Profil::get_diag(){
     return this->D;
 }
 
-void Factorisation::displayMatrix(Matrix M){
+Vector Profil::get_APi(){
+    return this->APi;
+}
+
+Vector Profil::get_num_diag(){
+    return this->nDiag;
+}
+
+Vector Profil::get_li(){
+    return this->li;
+}
+
+Vector Profil::get_pi(){
+    return this->pi;
+}
+
+void Profil::displayMatrix(Matrix M){
     for(int i=0;i<int(M.size());i++){
         for(int j=0;j<int(M[i].size());j++){
             cout<<M[i][j]<<"          ";
@@ -56,14 +81,14 @@ void Factorisation::displayMatrix(Matrix M){
     cout<<endl;
 }
 
-void Factorisation::displayVector(Vector V){
+void Profil::displayVector(Vector V){
     for(int j=0;j<int(V.size());j++){
         cout<<V[j]<<"  ";
     }
 }
 
-void Factorisation::initializeData(){
-    ifstream file("data.txt");
+void Profil::initializeData(){
+    ifstream file("data1.txt");
     if(file){
         file >> this->n;
         for(int i=0;i<this->n;i++){
@@ -91,7 +116,7 @@ void Factorisation::initializeData(){
     }
 }
 
-void Factorisation::decomposition(){
+void Profil::decomposition(){
     for(int i=0;i<int(A.size());i++){
         float sum = A[i][i];
         for(int k=0;k<i;k++){
@@ -114,7 +139,7 @@ void Factorisation::decomposition(){
 
 //Matrice triangulaire inférieur
 
-void Factorisation::lower_resolution(){
+void Profil::lower_resolution(){
     for(int i=0;i<this->n;i++){
         float sum = 0;
         for(int j=0;j<i;j++){
@@ -126,7 +151,7 @@ void Factorisation::lower_resolution(){
 
 //Matrice triangulaire supérieur
 
-void Factorisation::upper_resolution(){
+void Profil::upper_resolution(){
     for(int i=this->n-1;i>=0;i--){
         float sum = 0;
         for(int j=i+1;j<this->n;j++){
@@ -136,22 +161,74 @@ void Factorisation::upper_resolution(){
     }
 }
 
+void Profil::stockage(){
+    int count = 1;
+    for(int i=0;i<this->n;i++){
+        int indice = 0;
+        bool passage = false;
+        for(int j=0;j<i+1;j++){ 
+            auto element = this->A[i][j]; 
+            //Numérote le diagonal 
+            if(i==j){
+                this->nDiag.push_back(count);
+            }
+
+            if(element != 0 || A[i][i] == 0){
+                indice = j;
+                passage = true;
+                this->APi.push_back(element);
+                count++;
+            }
+            else{
+                if(j>indice && passage){
+                    this->APi.push_back(element);
+                    count++;
+                }
+            }
+        }
+
+        //Calcul de l[i] = nDiag[i] - nDiag[i-1] - 1
+        this->li.push_back(this->nDiag[i] - this->nDiag[i-1] - 1);
+
+        //Calcul de p[i] = i - l[i]
+        this->pi.push_back(i + 1 - this->li[i]);
+    }
+}
+
 int main(){
-    Factorisation f;
+    Profil p;
     cout<<"Voici la matrice donnee :"<<endl;
-    f.displayMatrix(f.get_matrix());
-    cout<<"Second membre :"<<endl;
-    f.displayVector(f.get_vector());
-    f.decomposition();
+    p.displayMatrix(p.get_matrix());
+    // cout<<"Second membre :"<<endl;
+    // p.displayVector(p.get_vector());
+    // p.decomposition();
+    // // cout<<endl;
+    // // cout<<"Diagonal :\n"<<endl;
+    // // p.displayVector(p.get_diag());
+    // p.lower_resolution();
+    // p.upper_resolution();
     // cout<<endl;
-    // cout<<"Diagonal :\n"<<endl;
-    // f.displayVector(f.get_diag());
-    f.lower_resolution();
-    f.upper_resolution();
+    // p.displayMatrix(p.get_matrix());
+    // cout<<"\nLa solution :"<<endl;
+    // p.displayVector(p.solution);
+    p.stockage();
+    cout<<"\nAPi : \n";
+    p.displayVector(p.get_APi());
     cout<<endl;
-    f.displayMatrix(f.get_matrix());
-    cout<<"\nLa solution :"<<endl;
-    f.displayVector(f.solution);
+    cout<<"nDiag :\n";
+    p.displayVector(p.get_num_diag());
+    cout<<endl;
+    cout<<"li :\n";
+    p.displayVector(p.get_li());
+    cout<<endl;
+    cout<<"pi :\n";
+    p.displayVector(p.get_pi());
+
+    p.decomposition();
+    p.lower_resolution();
+    p.upper_resolution();
+    cout<<"\n----------------"<<endl;
+    p.displayVector(p.solution);
     
     
 
